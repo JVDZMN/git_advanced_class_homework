ea48db184ef860b3d2a086aa28d4f4dacb54216e
Javad zamani <jdhj@kjkv.ld>
2018-09-27 10:16:26 +0200

    I have changed the math.js file

--- math.js
+++ math.js
@@ -7,8 +7,12 @@ const subtractNumbers = function(a,b) {
   return a-b;
 };
 
+const divisionToNumbers =(a,b) =>{
+  return a/b;
+}
 
 console.log(addNumbers());
 
 console.log(subtractNumbers());
 
+console.log(divisionToNumbers(200,5));
